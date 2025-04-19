382cf16183fb521f84dab23792453ec6a22351e2
davaleba-2 <erisiaduke>
2025-04-19 16:55:57 +0400

    finished log-in page

--- src/pages/log-in/log-in.jsx
+++ src/pages/log-in/log-in.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './log-in.css'
+import React from "react";
+import "./log-in.css";
+import { Link } from 'react-router-dom'
 
 function Log_in() {
   return (
-    <div>log-in</div>
-  )
+    <div className="log-in">
+      <div className="img-container">
+        <img src="src\pic/dl.beatsnoop 1.png" alt="" className="img" />
+      </div>
+      <div className="info">
+        <h2 className="title">Log in to Exclusive</h2>
+        <p className="description">Enter your details below</p>
+        <input type="text" placeholder="Email or Phone Number"  className="username"/>
+        <br />
+        <input type="text" placeholder="password"  className="password"/>
+        <br />
+        <button className="log-in-btn">Log In</button>
+        <Link to="/forgot-password" className="forgot-password">forgot password?</Link>
+      </div>
+    </div>
+  );
 }
 
-export default Log_in
+export default Log_in;
