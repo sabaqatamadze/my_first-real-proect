372cfae4eeba494593e439bc1afdf2a8eaed0d5d
davaleba-2 <erisiaduke>
2025-04-29 15:30:24 +0400

    add and finished not found page

--- src/pages/404 Error/404_Error.jsx
+++ src/pages/404 Error/404_Error.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './404_Error.css'
+import { Link } from 'react-router-dom'
 
 function Error() {
   return (
-    <div>404_Error</div>
+    <div className='error'>
+      <div className='link'>
+        <span className='home'>Home</span> / <span clas>404-Error</span>
+      </div>
+      <div className='text'>
+        <p className='not-found'>404 Not Found</p>
+      <p className='back-to-home'>Your visited page not found. You may go home page.</p>
+
+        <Link to={"/"} className='back-home-page'>Back to home page</Link>
+      </div>
+
+    </div>
   )
 }
 
